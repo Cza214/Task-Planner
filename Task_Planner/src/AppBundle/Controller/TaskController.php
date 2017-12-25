891f7a7752b36bd157a93b75b8b5f636988ac7c1
Cza214 <cza2-14@o2.pl>
2017-12-25 15:32:22 +0100

    Prepared Base Task Actions and DQL.

---
+++ Task_Planner/src/AppBundle/Controller/TaskController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Task;
+use AppBundle\Repository\TaskRepository;
+
+/**
+ * Class TaskController
+ * @package AppBundle\Controller
+ * @Route("tasks")
+ */
+class TaskController extends Controller
+{
+    /**
+     * @Route("/", name="show_tasks")
+     */
+    public function showAction(){
+        $current = new \DateTime('now');
+        $months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+        $tasks = $this->numberOfTasksByMonths();
+        dump($tasks);
+        return $this->render('AppBundle:Task:months.html.twig',['months' => $months, 'current' => $current, 'count' => $tasks]);
+    }
+
+    /**
+     * Get Number of Tasks by Month
+     */
+    protected function numberOfTasksByMonths(){
+
+        $em = $this->getDoctrine()->getManager();
+        $result = [];
+
+        for($i = 1; $i <= 12; $i++) {
+            $count = $em->getRepository('AppBundle:Task')->countByMonths($i,"2017");
+            dump($count);
+            $result[$i - 1] = count($count);
+        }
+        return $result;
+    }
+
+    /**
+     * Get Number of Tasks by Day
+     */
+    protected function numberOfTasksByDay(){
+
+    }
+}
---
+++ Task_Planner/src/AppBundle/Entity/Task.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="task")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetimetz")
+     */
+    private $date;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="priority", type="integer")
+     */
+    private $priority;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Task
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Task
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set priority
+     *
+     * @param integer $priority
+     *
+     * @return Task
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * Get priority
+     *
+     * @return int
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+}
+
